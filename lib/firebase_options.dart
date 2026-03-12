2f452977adce8e23b7dd6e410db20e47660ba99b
Ekraj Ghimire <56933759+ManishGhimire127@users.noreply.github.com>
2026-03-12 22:54:09 +1100

    updates

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -5,11 +5,11 @@ import 'package:firebase_core/firebase_core.dart';
 /// Alternatively, run `flutterfire configure` to generate this file automatically.
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform => const FirebaseOptions(
-        apiKey: 'YOUR_API_KEY',
-        appId: 'YOUR_APP_ID',
-        messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-        projectId: 'YOUR_PROJECT_ID',
-        authDomain: 'YOUR_AUTH_DOMAIN',
-        storageBucket: 'YOUR_STORAGE_BUCKET',
+        apiKey: 'AIzaSyC7tcIAIYL6Pvpx-KrwrYIZLawshfiW5pY',
+        appId: '1:623936773118:web:3ead17b46d2ce7cb7b7eaf',
+        messagingSenderId: '623936773118',
+        projectId: 'quickchat-6ab50',
+        authDomain: 'quickchat-6ab50.firebaseapp.com',
+        storageBucket: 'quickchat-6ab50.firebasestorage.app',
       );
 }
